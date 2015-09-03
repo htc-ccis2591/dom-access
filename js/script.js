e04b9660f2250b8bddd2ced11cc583f78247b47b
imdjsarge <rgallo001@my.hennepintech.edu>
2015-09-02 21:47:39 -0500

    more changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var images = document.querySelectorAll("#menu img");
 for (i = 0; i < images.length; i++) {
     heading = images[i]
     heading.setAttribute("class", "hide")
+    heading.onclick = function () {
+        alert('clicked the button');
+    }
 }
-
-OnClick 
