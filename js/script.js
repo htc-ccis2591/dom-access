8f3e5dfb9a70591c8c92a9d37e9c00706b618544
ggauchan001 <ggauchan001@users.noreply.github.com>
2015-09-02 21:37:04 -0500

    Dom Access

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 
+var content = document.getElementById("content");
+console.log(content);
+
+console.log("This is the start");
+
+var image = document.querySelectorAll("#menu img");
+
+for (i = 0; i<image.length; i++)
+{
+picture = image[i]
+
+picture.setAttribute("class", "hide");
+}
+
+console.log("the picture disapper");
+
+var onclick = document.querySelectorAll("
+
+
+
+
+
+
+
+
+
+
+
+
