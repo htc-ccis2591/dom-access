f6978e553d00885c55160894e549317da3898c32
unknown <rj2189guse@gmail.com>
2015-09-02 21:58:22 -0500

    Working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+var h3s = document.querySelectorAll("#menu h3");
 
+for(i=0; i< h3s.length; i++) {
+    oneHeading = h3s[i]
+    oneHeading.setAttribute("class", "hide");
+}
+var img1 = document.querySelectorAll("#menu item img");
+
+for(1=0; i< img1.length; i++) {
+    firstImage = img1[i]
+    firstImage.setAttribute("class", "hide");
