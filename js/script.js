45a90107f13e69620801246216e0d5e091be5011
imdjsarge <rgallo001@my.hennepintech.edu>
2015-09-07 23:35:33 -0500

    working with js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var images = document.querySelectorAll("#menu img");
 
 for (i = 0; i < images.length; i++) {
-    heading = images[i]
-    heading.setAttribute("class", "hide")
-    heading.onclick = function () {
-        alert('clicked the button');
-    }
+
+    var image = images[i];
+    image.setAttribute("class", "hide");
+
+    var target = document.getElementById("aside");
+    var p = document.createElement(image);
+    aside.appendChild(p);
+    
+ 
 }
