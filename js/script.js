ccfeb622cc9ad0bc223e6422c7b3b1096d44ed2f
antonettearabiana2 <tonette.arabiana@yahoo.com>
2015-09-07 18:07:41 -0700

    add dom-access

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+var imgs = document.querySelectorAll("#menu img");
+for (var i = 0; i < imgs.length; i++) 
+{
+    oneImage = imgs[i];
+    oneImage.setAttribute("class", "hide");
+    oneImage.previousElementSibling.addEventListener("click", hideshow, false);
+}
+
+
+function hideshow()
+{
+    var asideimage = document.getElementById('aside-image');
+    var imagepath=this.nextElementSibling.getAttribute("src");
+    asideimage.setAttribute("src",imagepath);
+    asideimage.removeAttribute("class");
+
+}
+
+
 
