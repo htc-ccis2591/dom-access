da3b893616adbea323549c6d8d24a9a0a9462e25
ggauchan001 <ggauchan001@users.noreply.github.com>
2015-09-09 19:51:59 -0500

    Final Version 2

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 
-var content = document.getElementById("content");
-console.log(content);
+var image = document.querySelectorAll("#menu img"),
+    imageCount = image.length,
+    i;
+var description = document.getElementsByTagName("li"),
+    desCount = description.length,
+    x,
+    picture = 0;
 
-console.log("This is the start");
-
-var image = document.querySelectorAll("#menu img");
-
-for (i = 0; i<image.length; i++)
-{
-picture = image[i]
-
-picture.setAttribute("class", "hide");
+for (i = 0; i < imageCount; i++) {
+    picture = image[i];
+    picture.setAttribute("class", "hide");
 }
 
 console.log("the picture disapper");
 
-var onclick = document.querySelectorAll("
+var text = "",
+    image = "",
+    asideImage = "",
+    li = 0;
+
+for (x = 0; x < desCount; x++) {
+    li = description[x];
+    text = li.firstChild;
+    image = li.lastChild;
 
 
+    text.onclick = function () {
+        asideImage = document.getElementById("aside-image");
+        asideImage.removeAttribute("class");
+        var path = this.nextElementSibling.getAttribute("src");
+        asideImage.setAttribute("src", path);
+    };
+}
+
+console.log("Bring the picture back on click");
 
 
+document.getElementById("Message").innerHTML = "Click menu names for pictures!";
 
-
-
-
-
-
-
-
+console.log("message to click for the picture");
