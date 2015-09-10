f6f42d3bb4626914769e83d7c1ce00a5ba90d8eb
SLSchmitt <lschmitt004@my.hennepintech.edu>
2015-09-09 19:57:50 -0500

    lori schmitt dom assgn.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+var i, addText, linkSrc, listImgs, imageLinks, imag, textTarget;
 
+listImgs = document.querySelectorAll("#menu img");
+imageLinks = document.getElementsByClassName("menu-item");
+textTarget = document.getElementById("espresso");
+
+var MenuOnClick = function () {
+    "use strict";
+    linkSrc = this.nextSibling;
+    document.getElementById("aside-image").setAttribute("class", "visible");
+    document.getElementById("aside-image").src = linkSrc.src;
+}
+
+for (i = 0; i < listImgs.length; i++) {
+    imag = listImgs[i];
+    imag.setAttribute("class", "hide");
+
+    imageLinks[i].addEventListener('click', MenuOnClick, false);
+}
+
+addText = document.createTextNode("Click a menu item to view a picture.");
+textTarget.parentNode.insertBefore(addText, textTarget);
