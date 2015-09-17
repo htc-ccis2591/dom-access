a6eac9965269931aff03aa9e2f9651f8d2f92488
unknown <htamiru001@my.hennepintech.edu>
2015-09-16 20:37:19 -0500

    assignment 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+document.getElementById("pic").setAttribute("class", "hidden");
+document.getElementById("pic1").setAttribute("class", "hidden");
+document.getElementById("pic2").setAttribute("class", "hidden");
+document.getElementById("pic3").setAttribute("class", "hidden");
+document.getElementById("pic4").setAttribute("class", "hidden");
+document.getElementById("pic5").setAttribute("class", "hidden");
+document.getElementById("pic6").setAttribute("class", "hidden");
+document.getElementById("pic7").setAttribute("class", "hidden");
+document.getElementById("pic8").setAttribute("class", "hidden");
+document.getElementById("pic9").setAttribute("class", "hidden");
+
+
+var target = document.getElementById("pic");
+   var p = document.createElement(Cappuccino);
+   aside.appendChild(p);
+
+
+
+
