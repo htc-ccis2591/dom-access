dc78d0f512ccf58bee9bbf3be2d975fe1997dc8c
chee-yang <cyang034@my.hennepintech.edu>
2016-09-08 19:47:01 -0500

    Menu Text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+var menuElement = document.getElementsByTagName("h2");
+var menuNav = document.createElement("div");
+var menuContext = document.createTextNode("Click on Images to Enlarge.");
 
+//if(menuElement.length > 0){
+    menuElement[0].appendChild(menuNav);
+    menuNav.appendChild(menuContext);
+//}
+
+var imgHide = document.getElementsByClassName("menu-item");
+var enlargedImages = document.createAttribute("class");
+
+var menuItems = document.getElementById("");
+var hideAttribute = document.createAttribute("class");
+hideAttribute.value = 'hide';
+menuItems.setAttributeNode(hideAttribute);
