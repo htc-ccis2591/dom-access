64830d5e57d9b7554cc8fa2ce8c7ef5b09cdedf2
chee-yang <cyang034@my.hennepintech.edu>
2016-09-13 15:21:24 -0500

    Currrent---

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+//Adding text to Menu
 var menuElement = document.getElementsByTagName("h2");
 var menuNav = document.createElement("div");
 var menuContext = document.createTextNode("Click on Images to Enlarge.");
 
-//if(menuElement.length > 0){
-    menuElement[0].appendChild(menuNav);
-    menuNav.appendChild(menuContext);
-//}
+menuElement[0].appendChild(menuNav);
+menuNav.appendChild(menuContext);
 
-var imgHide = document.getElementsByClassName("menu-item");
-var enlargedImages = document.createAttribute("class");
-
-var menuItems = document.getElementById("");
-var hideAttribute = document.createAttribute("class");
+//Hiding Images
+var imgDiv = document.createAttribute("div");
+var menuImages = document.getElementsByTagName("img");
+var hideAttribute = document.createAttribute("hide");
 hideAttribute.value = 'hide';
-menuItems.setAttributeNode(hideAttribute);
+
+
+menuImages[1].setAttributeNode(hideAttribute);
+
+
+//var hideMenuImages = function (){
+//    document.getElementsByTagName("img").style.visibility = "hidden";
+//}
