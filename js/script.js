b2719a78afc527980ce15052beb29e98595ca6cc
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-14 13:22:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function hideImage() {
   var image = document.selectElementsByTagName('img');
   var classAttribute = document.createAttribute("class");
-  classAttribute.value = 'display-hide';
+  classAttribute.value = 'hide';
   image.setAttributNote(classAttribute);
 }
 
