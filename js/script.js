947325fabda975831ad3a7474b627f56ef777287
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-14 15:38:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-function hideImage() {
-  var image = document.selectElementsByTagName('img');
-  var classAttribute = document.createAttribute("class");
-  classAttribute.value = 'hide';
-  image.setAttributNote(classAttribute);
+(function (){
+var textnode = document.createTextNode("Click on a menu item to see the image!");
+var menu = document.getElementById('menu');
+var image = menu.getElementsByTagName('img');
+var span = menu.getElementsByTagName('span');
+
+for (var i = 0; i < image.length; i++) {
+    var classAttribute = document.createAttribute("class");
+    classAttribute.value = 'hide';
+    image[i].setAttributeNode(classAttribute);
 }
 
-hideImage();
+
+menu.appendChild(textnode);
+menu.insertBefore(textnode, menu.childNodes[2]);
+
+}());
